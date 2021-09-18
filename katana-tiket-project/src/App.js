09b2805c47c5e30100928cc33a237b0f49631807
wahyuromadhontrees <wahyu.romadhon@treessolutions.com>
2021-09-18 20:06:32 +0700

    test

--- katana-tiket-project/src/App.js
+++ katana-tiket-project/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        Hello, selamat datang di Katana Tiket Online!
+        Hello, selamat datang di Katana Tiket Online!!
     </div>
   );
 }
